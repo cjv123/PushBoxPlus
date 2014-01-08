dd3665d93a966de4e5bffabf718527e7df6e8988
cjv123 <cjv123@qq.com>
2014-01-08 14:50:18 +0800

    安卓编译mk文件更改

--- proj.android/jni/Android.mk
+++ proj.android/jni/Android.mk
@@ -2,14 +2,19 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-include $(LOCAL_PATH)/../../Classes/file.mk
-
 LOCAL_MODULE := cocos2dcpp_shared
 
 LOCAL_MODULE_FILENAME := libcocos2dcpp
 
-LOCAL_SHORT_COMMANDS := true
-LOCAL_SRC_FILES += hellocpp/main.cpp $(MY_SRC_FILES)
+define walk 
+$(wildcard $(1)) $(foreach e, $(wildcard $(1)/*), $(call walk, $(e))) 
+endef 
+# 遍历Classes目录 
+ALLFILES = $(call walk, $(LOCAL_PATH)/../../Classes) 
+FILE_LIST := hellocpp/main.cpp 
+# 从所有文件中提取出所有.cpp文件 
+FILE_LIST += $(filter %.cpp, %.c, $(ALLFILES)) 
+LOCAL_SRC_FILES := $(FILE_LIST:$(LOCAL_PATH)/%=%) 
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
 
